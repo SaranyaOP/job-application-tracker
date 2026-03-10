9e9e0920949e5414e46758da74e50aa4dc6b44a0
SaranyaOP <saranyaop98@gmail.com>
2026-03-10 15:55:30 +0530

    second commit

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Sign In Page</h1>
+    </div>
+  );
+}
